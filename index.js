35369c246e2671ee97effbe15db2d2ef6a6e640f
Md-arefin <ansariarefin617@gmail.com>
2023-05-03 01:09:03 +0600

    added cors

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
 
+app.use(cors());
 
+app.get('/', (req, res) => {
+    res.send("Turkish food Cuisine")
+});
+
+app.listen(port, ()=> {
+    console.log(`Turkish food Cuisine on port:${port}`)
+})
